deed9fe6b79caa78844ade2d9cba43fa76efba72
StarXAi Enterprises <147140857+starxai@users.noreply.github.com>
2024-11-13 13:23:09 +0530

    updated BD URL

--- src/context/axiox.js
+++ src/context/axiox.js
@@ -1,5 +1,6 @@
 const isLocal = false;
 
-const Context = isLocal ? "http://groomerloadbalancer-1779385022.ap-south-1.elb.amazonaws.com/api" : "http://groomerloadbalancer-1779385022.ap-south-1.elb.amazonaws.com/api";
+const Context = isLocal ? "http://groomerloadbalancer-1779385022.ap-south-1.elb.amazonaws.com/api" 
+                        : "http://127.0.0.1:8000";
 
 export default Context;
